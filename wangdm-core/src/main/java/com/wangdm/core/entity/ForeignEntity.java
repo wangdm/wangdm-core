38250e028d6659ef0d1233817f1bfc9c6f56ea97
wangdm <wangdm1989@gmail.com>
2016-09-04 23:31:37 +0800

    wangdm-core 添加 ForeignEntity

---
+++ wangdm-core/src/main/java/com/wangdm/core/entity/ForeignEntity.java
@@ -0,0 +1,31 @@
+package com.wangdm.core.entity;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+import com.wangdm.core.constant.EntityStatus;
+
+@MappedSuperclass
+public abstract class ForeignEntity extends Pojo implements Entity {
+
+	private static final long serialVersionUID = 558471171157871925L;
+	
+    @Column(name="status", nullable=false)
+    private EntityStatus status = EntityStatus.UNAURHORIZED;
+
+	@Override
+	public void setId(Long id) {
+		
+	}
+
+    @Override
+    public EntityStatus getStatus() {
+        return status;
+    }
+
+    @Override
+    public void setStatus(EntityStatus status) {
+        this.status = status;
+    }
+
+}
